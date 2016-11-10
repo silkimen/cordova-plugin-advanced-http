9f515d01d8bb5dcb7cff82f41cacc50b8614cc1e
Sefa Ilkimen <sefa.ilkimen@mobisys.de>
2016-11-10 18:46:30 +0100

    defined www interface for configurable parameter serializer

--- www/cordovaHTTP.js
+++ www/cordovaHTTP.js
@@ -1,14 +1,40 @@
 /*global angular*/
 
 /*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * Modified by Andrew Stephan for Sync OnSet
+ * Modified by Sefa Ilkimen:
+ *  - added configurable params serializer
+ *
+*/
+
+/*
  * An HTTP Plugin for PhoneGap.
  */
 
 var exec = require('cordova/exec');
+var validSerializers = ['urlencoded', 'json'];
 
 // Thanks Mozilla: https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding#The_.22Unicode_Problem.22
 function b64EncodeUnicode(str) {
-    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
         return String.fromCharCode('0x' + p1);
     }));
 }
@@ -25,68 +51,65 @@ function mergeHeaders(globalHeaders, localHeaders) {
     return localHeaders;
 }
 
+function checkSerializer(serializer) {
+  serializer = serializer || '';
+  serializer = serializer.trim().toLowerCase();
+
+  if (validSerializers.indexOf(serializer) > -1) {
+    return serializer;
+  }
+
+  return serializer[0];
+}
+
 var http = {
     headers: {},
+    paramSerializer: 'urlencoded',
     sslPinning: false,
-    getBasicAuthHeader: function(username, password) {
+    getBasicAuthHeader: function (username, password) {
         return {'Authorization': 'Basic ' + b64EncodeUnicode(username + ':' + password)};
     },
-    useBasicAuth: function(username, password) {
+    useBasicAuth: function (username, password) {
         this.headers.Authorization = 'Basic ' + b64EncodeUnicode(username + ':' + password);
     },
-    setHeader: function(header, value) {
+    setHeader: function (header, value) {
         this.headers[header] = value;
     },
-    enableSSLPinning: function(enable, success, failure) {
-        return exec(success, failure, "CordovaHttpPlugin", "enableSSLPinning", [enable]);
+    setParamSerializer: function (serializer) {
+      this.paramSerializer = checkSerializer(serializer);
     },
-    acceptAllCerts: function(allow, success, failure) {
-        return exec(success, failure, "CordovaHttpPlugin", "acceptAllCerts", [allow]);
+    enableSSLPinning: function (enable, success, failure) {
+        return exec(success, failure, 'CordovaHttpPlugin', 'enableSSLPinning', [enable]);
     },
-    validateDomainName: function(validate, success, failure) {
-        return exec(success, failure, "CordovaHttpPlugin", "validateDomainName", [validate]);
+    acceptAllCerts: function (allow, success, failure) {
+        return exec(success, failure, 'CordovaHttpPlugin', 'acceptAllCerts', [allow]);
     },
-    post: function(url, params, headers, success, failure) {
+    validateDomainName: function (validate, success, failure) {
+        return exec(success, failure, 'CordovaHttpPlugin', 'validateDomainName', [validate]);
+    },
+    post: function (url, params, headers, success, failure) {
+        params = params || {};
+        headers = headers || {};
         headers = mergeHeaders(this.headers, headers);
-        return exec(success, failure, "CordovaHttpPlugin", "post", [url, params, headers]);
+        return exec(success, failure, 'CordovaHttpPlugin', 'post', [url, params, this.serializer, headers]);
     },
-    get: function(url, params, headers, success, failure) {
+    get: function (url, params, headers, success, failure) {
+        params = params || {};
+        headers = headers || {};
         headers = mergeHeaders(this.headers, headers);
-        return exec(success, failure, "CordovaHttpPlugin", "get", [url, params, headers]);
+        return exec(success, failure, 'CordovaHttpPlugin', 'get', [url, params, headers]);
     },
-    head: function(url, params, headers, success, failure) {
+    head: function (url, params, headers, success, failure) {
         headers = mergeHeaders(this.headers, headers);
-        return exec(success, failure, "CordovaHttpPlugin", "head", [url, params, headers]);
+        return exec(success, failure, 'CordovaHttpPlugin', 'head', [url, params, headers]);
     },
-    uploadFile: function(url, params, headers, filePath, name, success, failure) {
+    uploadFile: function (url, params, headers, filePath, name, success, failure) {
         headers = mergeHeaders(this.headers, headers);
-        return exec(success, failure, "CordovaHttpPlugin", "uploadFile", [url, params, headers, filePath, name]);
+        return exec(success, failure, 'CordovaHttpPlugin', 'uploadFile', [url, params, headers, filePath, name]);
     },
-    downloadFile: function(url, params, headers, filePath, success, failure) {
-        /*
-         *
-         * Licensed to the Apache Software Foundation (ASF) under one
-         * or more contributor license agreements.  See the NOTICE file
-         * distributed with this work for additional information
-         * regarding copyright ownership.  The ASF licenses this file
-         * to you under the Apache License, Version 2.0 (the
-         * "License"); you may not use this file except in compliance
-         * with the License.  You may obtain a copy of the License at
-         *
-         *   http://www.apache.org/licenses/LICENSE-2.0
-         *
-         * Unless required by applicable law or agreed to in writing,
-         * software distributed under the License is distributed on an
-         * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-         * KIND, either express or implied.  See the License for the
-         * specific language governing permissions and limitations
-         * under the License.
-         *
-         * Modified by Andrew Stephan for Sync OnSet
-         *
-        */
+    downloadFile: function (url, params, headers, filePath, success, failure) {
         headers = mergeHeaders(this.headers, headers);
-        var win = function(result) {
+        var win = function (result) {
             var entry = new (require('cordova-plugin-file.FileEntry'))();
             entry.isDirectory = false;
             entry.isFile = true;
@@ -96,75 +119,78 @@ var http = {
             entry.nativeURL = result.file.nativeURL;
             success(entry);
         };
-        return exec(win, failure, "CordovaHttpPlugin", "downloadFile", [url, params, headers, filePath]);
+        return exec(win, failure, 'CordovaHttpPlugin', 'downloadFile', [url, params, headers, filePath]);
     }
 };
 
 module.exports = http;
 
-if (typeof angular !== "undefined") {
-    angular.module('cordovaHTTP', []).factory('cordovaHTTP', function($timeout, $q) {
+if (typeof angular !== 'undefined') {
+    angular.module('cordovaHTTP', []).factory('cordovaHTTP', function ($timeout, $q) {
         function makePromise(fn, args, async) {
             var deferred = $q.defer();
-            
-            var success = function(response) {
+
+            var success = function (response) {
                 if (async) {
-                    $timeout(function() {
+                    $timeout(function () {
                         deferred.resolve(response);
                     });
                 } else {
                     deferred.resolve(response);
                 }
             };
-            
-            var fail = function(response) {
+
+            var fail = function (response) {
                 if (async) {
-                    $timeout(function() {
+                    $timeout(function () {
                         deferred.reject(response);
                     });
                 } else {
                     deferred.reject(response);
                 }
             };
-            
+
             args.push(success);
             args.push(fail);
-            
+
             fn.apply(http, args);
-            
+
             return deferred.promise;
         }
-        
+
         var cordovaHTTP = {
             getBasicAuthHeader: http.getBasicAuthHeader,
-            useBasicAuth: function(username, password) {
+            useBasicAuth: function (username, password) {
                 return http.useBasicAuth(username, password);
             },
-            setHeader: function(header, value) {
+            setHeader: function (header, value) {
                 return http.setHeader(header, value);
             },
-            enableSSLPinning: function(enable) {
+            setParamSerializer: function (serializer) {
+                return http.setParamSerializer(serializer);
+            },
+            enableSSLPinning: function (enable) {
                 return makePromise(http.enableSSLPinning, [enable]);
             },
-            acceptAllCerts: function(allow) {
+            acceptAllCerts: function (allow) {
                 return makePromise(http.acceptAllCerts, [allow]);
             },
-            validateDomainName: function(validate) {
+            validateDomainName: function (validate) {
                 return makePromise(http.validateDomainName, [validate]);
             },
-            post: function(url, params, headers) {
+            post: function (url, params, headers) {
                 return makePromise(http.post, [url, params, headers], true);
             },
-            get: function(url, params, headers) {
+            get: function (url, params, headers) {
                 return makePromise(http.get, [url, params, headers], true);
             },
-            head: function(url, params, headers) {
+            head: function (url, params, headers) {
                 return makePromise(http.head, [url, params, headers], true);
             },
-            uploadFile: function(url, params, headers, filePath, name) {
+            uploadFile: function (url, params, headers, filePath, name) {
                 return makePromise(http.uploadFile, [url, params, headers, filePath, name], true);
             },
-            downloadFile: function(url, params, headers, filePath) {
+            downloadFile: function (url, params, headers, filePath) {
                 return makePromise(http.downloadFile, [url, params, headers, filePath], true);
             }
         };
