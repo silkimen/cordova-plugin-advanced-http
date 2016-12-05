20e5bd0ee932b7b0fc6c411c911119d27688c54a
Sefa Ilkimen <sefa.ilkimen@mobisys.de>
2016-12-05 17:52:20 +0100

    added local storage store for cookies

--- www/cookie-handler.js
+++ www/cookie-handler.js
@@ -1,6 +1,8 @@
 var pluginId = module.id.slice(0, module.id.indexOf('.'));
 var ToughCookie = require(pluginId + '.tough-cookie');
-var cookieJar = new ToughCookie.CookieJar();
+var WebStorageCookieStore = require(pluginId + '.local-storage-store');
+var store = new WebStorageCookieStore();
+var cookieJar = new ToughCookie.CookieJar(store);
 
 module.exports = {
     setCookie: setCookie,
