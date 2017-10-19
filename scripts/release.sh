d136d90893c1367d8fe5b903318ec482f65744be
Sefa Ilkimen <sefa.ilkimen@mobisys.de>
2017-10-19 17:56:30 +0200

    - improved release process (using npm script) - updated version number

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-VERSION=$(node -e "console.log(require('./package.json').version)")
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 
+pushd $ROOT
+VERSION=$(node -e "console.log(require('./package.json').version)")
 ./scripts/update-tough-cookie.sh
 node ./scripts/update-plugin-xml.js $VERSION
 git commit -a -m "release v$VERSION"
 git tag "v$VERSION"
 npm publish
+popd
