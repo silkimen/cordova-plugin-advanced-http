8c596d80b053f403ac8a381b8ecf07bb31d0b4e4
Jochen Becker <jochen.becker@mobisys.de>
2017-10-26 11:27:30 +0200

    fixed global 'asterisks' header not defined

--- www/advanced-http.js
+++ www/advanced-http.js
@@ -151,6 +151,7 @@ var http = {
         return {'Authorization': 'Basic ' + b64EncodeUnicode(username + ':' + password)};
     },
     useBasicAuth: function (username, password) {
+        this.headers['*'] = this.headers['*'] || {};
         this.headers['*'].Authorization = 'Basic ' + b64EncodeUnicode(username + ':' + password);
     },
     setHeader: function () {
