b60086df0dade7f8a1253e8df5423da24603e158
Thomas Opiolka <opiolka.thomas@googlemail.com>
2017-11-08 11:16:42 +0100

    Add support for Content-Type application/javascript

--- src/ios/TextResponseSerializer.m
+++ src/ios/TextResponseSerializer.m
@@ -23,7 +23,7 @@ static BOOL AFErrorOrUnderlyingErrorHasCodeInDomain(NSError *error, NSInteger co
         return nil;
     }
 
-    self.acceptableContentTypes = [NSSet setWithObjects:@"text/plain", @"text/html", @"text/json", @"application/hal+json", @"application/json", @"text/xml", @"application/xml", @"text/css", nil];
+    self.acceptableContentTypes = [NSSet setWithObjects:@"text/plain", @"text/html", @"text/json", @"application/hal+json", @"application/json", @"text/xml", @"application/xml", @"text/css", @"application/javascript", nil];
 
     return self;
 }
