912cb2b877cecd4a27a6c764500390f2a0972aa2
Izam Basiron <izam.shah@gmail.com>
2020-06-15 18:33:30 +0800

    Remove okhttp dependency

--- src/android/com/silkimen/http/OkConnectionFactory.java
+++ src/android/com/silkimen/http/OkConnectionFactory.java
@@ -1,26 +1,27 @@
 package com.silkimen.http;
 
-import okhttp3.OkHttpClient;
-import okhttp3.OkUrlFactory;
-
+import java.io.IOException;
 import java.net.URL;
 import java.net.HttpURLConnection;
-import java.net.URLStreamHandler;
 import java.net.Proxy;
 
 public class OkConnectionFactory implements HttpRequest.ConnectionFactory {
-  private final OkHttpClient client = new OkHttpClient();
 
   public HttpURLConnection create(URL url) {
-    OkUrlFactory urlFactory = new OkUrlFactory(this.client);
-
-    return (HttpURLConnection) urlFactory.open(url);
+      try {
+          return (HttpURLConnection) url.openConnection();
+      } catch (IOException e) {
+          e.printStackTrace();
+          return null;
+      }
   }
 
   public HttpURLConnection create(URL url, Proxy proxy) {
-    OkHttpClient clientWithProxy = new OkHttpClient.Builder().proxy(proxy).build();
-    OkUrlFactory urlFactory = new OkUrlFactory(clientWithProxy);
-
-    return (HttpURLConnection) urlFactory.open(url);
+      try {
+          return (HttpURLConnection) url.openConnection(proxy);
+      } catch (IOException e) {
+          e.printStackTrace();
+          return null;
+      }
   }
 }
