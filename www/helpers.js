1c09ba19f51e424627ef4a8790204f0e1680089c
Graham Miller <Graham_Miller@insideranken.org>
2020-08-01 21:21:32 -0500

    fix format

--- www/helpers.js
+++ www/helpers.js
@@ -7,23 +7,23 @@ module.exports = function init(global, jsUtil, cookieHandler, messages, base64, 
 
   var interface = {
     b64EncodeUnicode: b64EncodeUnicode,
-      checkClientAuthMode: checkClientAuthMode,
-      checkClientAuthOptions: checkClientAuthOptions,
-      checkDownloadFilePath: checkDownloadFilePath,
-      checkFollowRedirectValue: checkFollowRedirectValue,
-      checkForBlacklistedHeaderKey: checkForBlacklistedHeaderKey,
-      checkForInvalidHeaderValue: checkForInvalidHeaderValue,
-      checkSerializer: checkSerializer,
-      checkSSLCertMode: checkSSLCertMode,
-      checkTimeoutValue: checkTimeoutValue,
-      checkUploadFileOptions: checkUploadFileOptions,
-      getMergedHeaders: getMergedHeaders,
-      processData: processData,
-      handleMissingCallbacks: handleMissingCallbacks,
-      handleMissingOptions: handleMissingOptions,
-      injectCookieHandler: injectCookieHandler,
-      injectFileEntryHandler: injectFileEntryHandler,
-      injectRawResponseHandler: injectRawResponseHandler,
+    checkClientAuthMode: checkClientAuthMode,
+    checkClientAuthOptions: checkClientAuthOptions,
+    checkDownloadFilePath: checkDownloadFilePath,
+    checkFollowRedirectValue: checkFollowRedirectValue,
+    checkForBlacklistedHeaderKey: checkForBlacklistedHeaderKey,
+    checkForInvalidHeaderValue: checkForInvalidHeaderValue,
+    checkSerializer: checkSerializer,
+    checkSSLCertMode: checkSSLCertMode,
+    checkTimeoutValue: checkTimeoutValue,
+    checkUploadFileOptions: checkUploadFileOptions,
+    getMergedHeaders: getMergedHeaders,
+    processData: processData,
+    handleMissingCallbacks: handleMissingCallbacks,
+    handleMissingOptions: handleMissingOptions,
+    injectCookieHandler: injectCookieHandler,
+    injectFileEntryHandler: injectFileEntryHandler,
+    injectRawResponseHandler: injectRawResponseHandler,
   };
 
   // expose all functions for testing purposes
@@ -148,8 +148,8 @@ module.exports = function init(global, jsUtil, cookieHandler, messages, base64, 
 
     // systemstore
     if (mode === validClientAuthModes[1]) {
-      if (jsUtil.getTypeOf(options.alias) !== 'String' &&
-        jsUtil.getTypeOf(options.alias) !== 'Undefined') {
+      if (jsUtil.getTypeOf(options.alias) !== 'String'
+        && jsUtil.getTypeOf(options.alias) !== 'Undefined') {
         throw new Error(messages.INVALID_CLIENT_AUTH_ALIAS);
       }
 
@@ -266,7 +266,7 @@ module.exports = function init(global, jsUtil, cookieHandler, messages, base64, 
   }
 
   function createFileEntry(rawEntry) {
-    var entry = new(require('cordova-plugin-file.FileEntry'))();
+    var entry = new (require('cordova-plugin-file.FileEntry'))();
 
     entry.isDirectory = rawEntry.isDirectory;
     entry.isFile = rawEntry.isFile;
@@ -297,14 +297,16 @@ module.exports = function init(global, jsUtil, cookieHandler, messages, base64, 
       try {
         // json
         if (responseType === validResponseTypes[1] && typeof response.data == 'string') {
-          response.data = response.data === '' ? undefined : JSON.parse(response.data);
+          response.data = response.data === ''
+            ? undefined
+            : JSON.parse(response.data);
         }
 
         // arraybuffer
         else if (responseType === validResponseTypes[2]) {
-          response.data = response.data === '' ?
-            null :
-            base64.toArrayBuffer(response.data);
+          response.data = response.data === ''
+            ? null
+            : base64.toArrayBuffer(response.data);
         }
 
         // blob
@@ -314,9 +316,7 @@ module.exports = function init(global, jsUtil, cookieHandler, messages, base64, 
           } else {
             var buffer = base64.toArrayBuffer(response.data);
             var type = response.headers['content-type'] || '';
-            var blob = new Blob([buffer], {
-              type: type
-            });
+            var blob = new Blob([buffer], { type: type });
             response.data = blob;
           }
         }
@@ -343,9 +343,7 @@ module.exports = function init(global, jsUtil, cookieHandler, messages, base64, 
     var cookieString = cookieHandler.getCookieString(url);
 
     if (cookieString.length) {
-      return {
-        Cookie: cookieHandler.getCookieString(url)
-      };
+      return { Cookie: cookieHandler.getCookieString(url) };
     }
 
     return {};
@@ -413,9 +411,7 @@ module.exports = function init(global, jsUtil, cookieHandler, messages, base64, 
 
     switch (dataSerializer) {
       case 'utf8':
-        return cb({
-          text: data
-        });
+        return cb({ text: data });
       case 'raw':
         return cb(currentDataType === 'Uint8Array' ? data.buffer : data);
       case 'multipart':
