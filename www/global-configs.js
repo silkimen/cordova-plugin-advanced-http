7c1836e87ffd0f19391ea7f2a35386ae753eb237
Moon <YouYue123@users.noreply.github.com>
2021-03-25 00:02:29 +0800

    Set default connectTimeout and readTimeout as 60s

--- www/global-configs.js
+++ www/global-configs.js
@@ -3,8 +3,8 @@ var globalConfigs = {
   serializer: 'urlencoded',
   followRedirect: true,
   timeout: 60.0,
-  connectTimeout: 30.0,
-  readTimeout: 30.0
+  connectTimeout: 60.0,
+  readTimeout: 60.0
 };
 
 module.exports = globalConfigs;
